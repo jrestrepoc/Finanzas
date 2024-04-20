d0e04a8298f962d2e9df979327e01040b9be46ce
Jerónimo Restrepo <jrestrepoc@eafit.edu.co>
2024-04-19 20:42:30 -0500

    Added Changes

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,5 @@ from core import views as coreViews
 
 urlpatterns = [
     path("", coreViews.front_view, name="front"),
+    path("acerca/", coreViews.acerca_view, name="acerca"),
 ]
--- core/views.py
+++ core/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def front_view(request):
     return render(request, 'index.html')
+
+def acerca_view(request):
+    return render(request, 'acerca.html')
